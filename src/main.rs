a1d28a3da7c8ddc9069847ca6e8c2578059f2ab0
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-11-20 16:36:37 +0530

    feat: the active tile is the one where the cursor is

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,70 @@
-extern crate nalgebra as na;
-
-use nalgebra::base::Vector2;
+use rand::Rng;
 use raylib::prelude::*;
+
+#[derive(Debug, Copy, Clone)]
+enum EntityType {
+    Bug,
+    Trap,
+    Empty,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum TileState {
+    Active,
+    Opened,
+    Closed,
+}
 
 #[derive(Debug, Copy, Clone)]
 struct Tile {
     x: i32,
     y: i32,
-    open: bool,
+    state: TileState,
+    entity: EntityType,
 }
 
 impl Tile {
     fn new(x: i32, y: i32) -> Self {
-        Tile { x, y, open: false }
+        Tile {
+            x,
+            y,
+            state: TileState::Closed,
+            entity: EntityType::Empty,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Bug {
+    x: i32,
+    y: i32,
+}
+
+impl Bug {
+    fn new(x: i32, y: i32) -> Self {
+        Bug { x, y }
     }
 }
 
 fn main() {
-    let screen_size: i32 = 800;
     let gap: i32 = 10;
     let no_of_rows: i32 = 10;
+    let screen_size: i32 = 800;
     let box_size: i32 = (screen_size - (no_of_rows + 1) * gap) / 10;
+    let mut rng = rand::thread_rng();
 
     let (mut rl, thread) = raylib::init().size(800, 800).title("Bugsweeper").build();
+    let mut bugs: [Bug; 5] = [Bug::new(0, 0); 5];
+
+    for i in 0..(bugs.len()) {
+        bugs[i].x = rng.gen::<i32>();
+        bugs[i].y = rng.gen::<i32>();
+    }
 
     rl.set_target_fps(60);
 
     let crimson = Color::from_hex("DC143C").unwrap();
-    let grey = Color::from_hex("000333").unwrap();
+    let grey = Color::from_hex("333333").unwrap();
 
     let mut board: [[Tile; 10]; 10] = [[Tile::new(0, 0); 10]; 10];
 
@@ -38,50 +75,69 @@ fn main() {
         }
     }
 
+    let mut intended_state = TileState::Closed;
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
+
+        let mouse_pos = d.get_mouse_position();
+        let mouse_x = mouse_pos.x as i32;
+        let mouse_y = mouse_pos.y as i32;
+
+        // if left click is pressed && mouse is over a tile 'A' then tile 'A' should turn to 'grey'
+        if d.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
+            intended_state = TileState::Active;
+        }
+
+        if d.is_mouse_button_released(MouseButton::MOUSE_LEFT_BUTTON) {
+            intended_state = TileState::Opened;
+        }
+
+        for i in 0..(no_of_rows as usize) {
+            let row = board[i];
+
+            for j in 0..(row.len()) {
+                let tile = row[j];
+                if mouse_x > tile.x
+                    && mouse_x < tile.x + box_size
+                    && mouse_y > tile.y
+                    && mouse_y < tile.y + box_size
+                {
+                    match board[i][j].state {
+                        TileState::Closed => board[i][j].state = intended_state,
+                        TileState::Active => board[i][j].state = intended_state,
+                        TileState::Opened => {}
+                    }
+                } else {
+                    match board[i][j].state {
+                        TileState::Closed => board[i][j].state = TileState::Closed,
+                        TileState::Active => board[i][j].state = TileState::Closed,
+                        TileState::Opened => {}
+                    }
+                }
+            }
+        }
+
+        println!("{:?}", mouse_pos);
 
         for row in board.iter() {
             for tile in row {
                 let x = tile.x;
                 let y = tile.y;
 
-                match tile.open {
-                    true => d.draw_rectangle(x, y, box_size, box_size, crimson),
-                    false => d.draw_rectangle(x, y, box_size, box_size, Color::BLACK),
+                match tile.state {
+                    TileState::Opened => d.draw_rectangle(x, y, box_size, box_size, crimson),
+                    TileState::Closed => d.draw_rectangle(x, y, box_size, box_size, Color::BLACK),
+                    TileState::Active => d.draw_rectangle(x, y, box_size, box_size, grey),
                 }
             }
         }
+
 
         if d.is_mouse_button_released(MouseButton::MOUSE_LEFT_BUTTON) {
-            let mouse_pos = d.get_mouse_position();
-            let mouse_x = mouse_pos.x as i32;
-            let mouse_y = mouse_pos.y as i32;
-
-            for i in 0..(no_of_rows as usize) {
-                let row = board[i];
-
-                for j in 0..(row.len()) {
-                    let tile = row[j];
-                    if mouse_x > tile.x
-                        && mouse_x < tile.x + box_size
-                        && mouse_y > tile.y
-                        && mouse_y < tile.y + box_size
-                    {
-                        board[i][j].open = !board[i][j].open;
-                    }
-                }
-            }
+            intended_state = TileState::Closed;
         }
-
-        d.draw_text(
-            format!("FPS: {:?}", d.get_mouse_position()).as_str(),
-            16,
-            16,
-            20,
-            Color::BLACK,
-        );
     }
 }
